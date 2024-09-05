e7c981884acb617f91b675003195f8831bce6a18
srivas-ITB <santiago.rivas@itb.cat>
2024-09-05 19:57:59 +0200

    Ordenación especiales

--- src/Cap2/_1_EstructurasDatosConBibliotecas/_7_Ordenacion_Especiales/Mali.java
+++ src/Cap2/_1_EstructurasDatosConBibliotecas/_7_Ordenacion_Especiales/Mali.java
@@ -3,20 +3,29 @@ package Cap2._1_EstructurasDatosConBibliotecas._7_Ordenacion_Especiales;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
+import Utils.Kattio;
 
 /*
  Al hacer los casos de prueba, se puede observar que el mínimo de A y el máximo de B son los que suman el valor más pequeño
- pero no pasa el caso #3 --> WA
+ pero no pasa el caso #3 --> WA  un caso donde no se cumple es
+ 2
+ 80 10
+ 15 50
+ Debe dar 90 y 90, pero este planteamiento da 90 y 65!
+
+ Deben buscarse los valores de A y B que sumen el valor más pequeño y eso se consigue sumando los valores más bajos de A con los más altos de B.
 
  Adaptando el código de C++
+
+ Caso #4: TLE --> Paso a usar Kattio --> AC
 
  */
 
 
 public class Mali {
 
-
-    public static void main(String[] args) {
+/*
+    public static void main_WA3(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         scan.useLocale(Locale.ENGLISH);
@@ -39,90 +48,65 @@ public class Mali {
 
     }
 }
-
-
-/*
-/*
-Kattis - Mali
-
-Observation 1:
-    the most optimal pairing is repeatedly greedily pairing the smallest of A with the largest of B.
-
-    proof:
-        From any pairing, we attempt to re-pair b[n] (largest b) with a[0] (currently paired with b[j]):
-            (a[0], b[j]) (a[i], b[n]) --> (a[0], b[n]) (a[i],b[j])
-            if (a[i], b[n]) is the max pairing:
-                re-pairing will not increase the max since
-                    b[n] + a[0] <= b[n]+a[i] because i>0
-                    a[i]+b[j] <= b[n]+a[i] because n>j
-
-            else if (a[i], b[n]) is not the max pairing:
-                re-pairing will not increase the max since
-                    the max pair involves an a[k], k>i since the max must be greater than (a[i], b[n]) and all other b values are less than b[n]
-                        thus k != 0
-                    let the max pair be (a[k], b[l])
-                    b[n]+a[0] <= b[n] + a[i] < a[k] + b[l] ; former because i > 0, latter because of assumption that initial pairing not max pair
-                    a[i]+b[j] <=b[n]+a[i]  < a[k] + b[l] former because n>j, latter because of assumption that initial pairing not max pair
-
-        Thus, we can safely make the pair (a[0], b[n]) and include them from our consideration,leaving us with n-1 a[i]s and b[i]s each.
-        Repeating the above logic n times proves the theorem
-
-
-    Design 1:
-        use a frequency arr instead of just an array of A and B as the range of values is [1,100], which is extremely small
-        this frequency array can also be used to do the matching (see details below)
-
-Time: O(n * 100) = O(n), Mem: O(n)
 */
-/*
-#include <bits/stdc++.h>
-using namespace std;
 
-int n;
-int f_a[101], f_b[101]; // f_a[i] is the number of is in a
 
-int compute_max_sum(int num){
-    int freq_a[101], freq_b[101];
-    for (int i=1;i<=100;i++){
-        freq_a[i] = f_a[i];
-        freq_b[i] = f_b[i];
-    }
-    int index_a = 1, index_b = 100;
-    int counter=0;
-    int max_sum = 0;
-    while(counter<num){
-        while(freq_b[index_b] == 0){
-            index_b--;
+    static int n;
+    static int[] f_a = new int[101];
+    static int[] f_b = new int[101]; // f_a[i] is the number of is in a
+
+    public static int compute_max_sum(int num) {
+        int[] freq_a = new int[101];
+        int[] freq_b = new int[101];
+        //Copiar las tablas de frecuencias
+        for (int i = 1; i <= 100; i++) {
+            freq_a[i] = f_a[i];
+            freq_b[i] = f_b[i];
         }
-        while (freq_a[index_a] == 0){
-            index_a++;
+
+        //Inicializar los valores de los índices y contadores
+        int index_a = 1, index_b = 100;
+        int counter = 0;
+        int max_sum = 0;
+
+        //Recorrer las tablas de frecuencias
+        while (counter < num) {
+            //Busco el valor de b más alto y el de a más bajo
+            while (freq_b[index_b] == 0) {
+                index_b--;
+            }
+            while (freq_a[index_a] == 0) {
+                index_a++;
+            }
+            //Optimización: determinar el número de elementos que resultan en parejas iguales
+            int match_num = Math.min(freq_a[index_a], freq_b[index_b]);
+
+            //Actualizo la tabla de frecuencias y los contadores
+            freq_b[index_b] -= match_num;
+            freq_a[index_a] -= match_num;
+
+            //Me quedo con el máximo de la suma de los índices de la tabla de frecuencias (que son los valores de a y b)
+            max_sum = Math.max(max_sum, index_b + index_a);
+            counter += match_num;
         }
-        int match_num = min(freq_a[index_a], freq_b[index_b]);
 
-        freq_b[index_b] -= match_num;
-        freq_a[index_a] -= match_num;
-
-        max_sum = max(max_sum, index_b+index_a);
-        counter += match_num;
+        return max_sum;
     }
 
-    return max_sum;
-}
+    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        scan.useLocale(Locale.ENGLISH);
+        Kattio scan = new Kattio(System.in, System.out);
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+        n = scan.getInt();
+        for (int r = 1; r <= n; r++) {
+            int a_c = scan.getInt();
+            int b_c = scan.getInt();
+            f_a[a_c]++;
+            f_b[b_c]++;
 
-    cin>>n;
-    for (int r=1;r<=n;r++){
-        int a_c, b_c;
-        cin >> a_c >> b_c;
-        f_a[a_c]++;
-        f_b[b_c]++;
-
-        cout << compute_max_sum(r) << endl;
+            System.out.println(compute_max_sum(r));
+        }
     }
 
-    return 0;
 }
- */

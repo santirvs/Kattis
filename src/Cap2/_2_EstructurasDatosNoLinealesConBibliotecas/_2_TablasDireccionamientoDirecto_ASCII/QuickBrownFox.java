050241d55c6b0a73f80d2c3b6ae161e90be619a6
srivas-ITB <santiago.rivas@itb.cat>
2024-09-28 13:48:48 +0200

    Acabados los de tabla de direccionamiento directo ASCII

---
+++ src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_2_TablasDireccionamientoDirecto_ASCII/QuickBrownFox.java
@@ -0,0 +1,47 @@
+package Cap2._2_EstructurasDatosNoLinealesConBibliotecas._2_TablasDireccionamientoDirecto_ASCII;
+
+// Contar las apariciones de cada letra (bastaría con un booleano)
+// Revisar las letras que no han aparecido
+// Si son todas -> pangrama
+// Sino, --> mostrar las que faltan
+
+import java.util.Scanner;
+
+public class QuickBrownFox {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        int numCasos = scan.nextInt();
+        scan.nextLine();
+
+        while (numCasos > 0 ) {
+
+            char[] cuentas = new char['Z' - 'A' + 1];
+
+            String cadena = scan.nextLine().toLowerCase();
+            for (int i = 0; i < cadena.length(); i++) {
+                char c = cadena.charAt(i);
+                if (c >= 'a' && c <= 'z') {
+                    cuentas[c - 'a']++;
+                }
+            }
+
+            String missing = "";
+            for (int i = 0; i < cuentas.length; i++) {
+                if (cuentas[i] == 0) {
+                    missing += (char)(i + 'a');
+                }
+            }
+
+            if (missing.isEmpty()) {
+                System.out.println("pangram");
+            } else {
+                System.out.println("missing " + missing);
+            }
+
+            numCasos--;
+        }
+
+    }
+}

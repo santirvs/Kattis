eb0d98ec9afe8fb117a423ebd2a3fdb46487d8c7
srivas-ITB <santiago.rivas@itb.cat>
2024-10-04 21:31:52 +0200

    Intentando DeduplicatingFiles

---
+++ src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_4_TablaDeHashSet/DeduplicatingFiles.java
@@ -0,0 +1,63 @@
+package Cap2._2_EstructurasDatosNoLinealesConBibliotecas._4_TablaDeHashSet;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+// Apuntar en un array los hashes de cada cadena
+// Caso 2: WA!
+
+
+
+public class DeduplicatingFiles {
+
+        public static int calcularHash(String cadena) {
+            int hash = 0;
+            for (int i=0; i<cadena.length(); i++) {
+                hash = hash ^ cadena.charAt(i);
+            }
+            return hash;
+        }
+
+       public static void main(String[] args)  {
+
+        Scanner scan = new Scanner(System.in);
+
+        int numFiles = scan.nextInt();
+
+
+        while (numFiles!=0) {
+            scan.nextLine();
+            String[] files = new String[numFiles];
+
+            //Leer los ficheros
+            for (int i = 0; i < numFiles; i++) {
+                files[i] = scan.nextLine();
+            }
+
+            //Calcular los hashes y las colisiones
+            long collisions = 0;
+            long repetidas = 0;
+            HashSet<Integer> hashes = new HashSet<>();
+            for (int i = 0; i < numFiles; i++) {
+                int hash = calcularHash(files[i]);
+                hashes.add(hash);
+                //Calcula el hash del resto de cadenas
+                for (int j = i + 1; j < numFiles; j++) {
+                    int hash2 = calcularHash(files[j]);
+                    if (hash == hash2) {
+                        if (files[i].equals(files[j])) {
+                            repetidas ++;
+                        }
+                        else
+                            collisions++;
+                    }
+                }
+            }
+
+            System.out.println((numFiles-repetidas) + " " + collisions);
+
+            numFiles = scan.nextInt();
+        }
+
+    }
+}

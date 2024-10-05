d500eab566c1070f044d8d0c42a557c96c4c5629
srivas-ITB <santiago.rivas@itb.cat>
2024-10-05 09:23:11 +0200

    Solucionado DeduplicatingFiles

--- src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_4_TablaDeHashSet/DeduplicatingFiles.java
+++ src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_4_TablaDeHashSet/DeduplicatingFiles.java
@@ -1,63 +1,87 @@
 package Cap2._2_EstructurasDatosNoLinealesConBibliotecas._4_TablaDeHashSet;
 
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 // Apuntar en un array los hashes de cada cadena
-// Caso 2: WA!
-
+// Caso 2: WA!  Adapto de https://github.com/mpfeifer1/Kattis/blob/master/deduplicatingfiles.cpp  --> AC
+//    PENDIENTE DE ANALIZAR!!!
 
 
 public class DeduplicatingFiles {
 
-        public static int calcularHash(String cadena) {
-            int hash = 0;
-            for (int i=0; i<cadena.length(); i++) {
-                hash = hash ^ cadena.charAt(i);
-            }
-            return hash;
-        }
 
-       public static void main(String[] args)  {
+    //    public static int getcollisions(unordered_map<int, unordered_map<string, int>> vals) {
+    public static int getcollisions(HashMap<Integer, HashMap<String, Integer>> vals) {
+        int total = 0;
 
-        Scanner scan = new Scanner(System.in);
-
-        int numFiles = scan.nextInt();
-
-
-        while (numFiles!=0) {
-            scan.nextLine();
-            String[] files = new String[numFiles];
-
-            //Leer los ficheros
-            for (int i = 0; i < numFiles; i++) {
-                files[i] = scan.nextLine();
+        // For each hash that landed here
+        for(HashMap i : vals.values()) {
+            // Get the count of each different string here
+            List counts = new ArrayList();
+            for(Object j : i.values()) {
+                counts.add(j);
             }
 
-            //Calcular los hashes y las colisiones
-            long collisions = 0;
-            long repetidas = 0;
-            HashSet<Integer> hashes = new HashSet<>();
-            for (int i = 0; i < numFiles; i++) {
-                int hash = calcularHash(files[i]);
-                hashes.add(hash);
-                //Calcula el hash del resto de cadenas
-                for (int j = i + 1; j < numFiles; j++) {
-                    int hash2 = calcularHash(files[j]);
-                    if (hash == hash2) {
-                        if (files[i].equals(files[j])) {
-                            repetidas ++;
-                        }
-                        else
-                            collisions++;
-                    }
+            // Calculate the total collisions
+            for(int j = 0; j < counts.size()-1; j++) {
+                for(int k = j+1; k < counts.size(); k++) {
+                    total += (int)counts.get(j) * (int)counts.get(k);
                 }
             }
-
-            System.out.println((numFiles-repetidas) + " " + collisions);
-
-            numFiles = scan.nextInt();
         }
+
+        return total;
+    }
+
+
+   public static int calcularHash(String cadena) {
+        int hash = cadena.charAt(0);
+        for (int i=1; i<cadena.length(); i++) {
+            hash = hash ^ cadena.charAt(i);
+        }
+        return hash;
+    }
+
+   public static void main(String[] args) {
+
+       Scanner scan = new Scanner(System.in);
+
+       int n = scan.nextInt();
+
+       while (n != 0) {
+           scan.nextLine();
+           HashSet<String> s = new HashSet<>();
+           HashMap<Integer, HashMap<String, Integer>> vals = new HashMap<>();
+
+           for (int i = 0; i < n; i++) {
+               // Read in sentence, add to set
+               String sentence = scan.nextLine();
+               s.add(sentence);
+
+               // Calculate hash, add it to list of hashes
+               int hash = calcularHash(sentence);
+               //vals[hash][sentence]++;
+               if (vals.containsKey(hash)) {
+                   if (vals.get(hash).containsKey(sentence)) {
+                       vals.get(hash).put(sentence, vals.get(hash).get(sentence) + 1);
+                   } else {
+                       vals.get(hash).put(sentence, 1);
+                   }
+               } else {
+                   HashMap<String, Integer> temp = new HashMap<>();
+                   temp.put(sentence, 1);
+                   vals.put(hash, temp);
+               }
+           }
+
+
+           int c = getcollisions(vals);
+           System.out.println(s.size() + " " + c);
+
+           n = scan.nextInt();
+       }
 
     }
 }
+
+

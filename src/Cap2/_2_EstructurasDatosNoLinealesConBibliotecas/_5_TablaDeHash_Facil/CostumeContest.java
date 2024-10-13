57d95ad1c315709ab7ecbedd340652ad8ae5804a
srivas-ITB <santiago.rivas@itb.cat>
2024-10-13 21:33:42 +0200

    Continuo los de Tabla de HashSet - Faciles

---
+++ src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_5_TablaDeHash_Facil/CostumeContest.java
@@ -0,0 +1,58 @@
+package Cap2._2_EstructurasDatosNoLinealesConBibliotecas._5_TablaDeHash_Facil;
+
+
+import java.util.*;
+
+// Leer las categorias y guardarlas en un HashMap (String, Int)
+// Si ya existe la categoria, incrementar el contador
+// Al final, recorrer el HashMap y ver cuantas categorias tienen el mismo número de votos
+// Guardarlas en una lista y mostrarlas ordenadas
+
+// Caso #4: WA  --> Faltaba ordenar la llista de categorias ganadoras --> AC
+
+
+public class CostumeContest {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int numAmigos = scan.nextInt();
+
+        TreeMap<String, Integer> recuento = new TreeMap<String, Integer>();
+
+        //Leer las categorias y guardarlas en el HashMap
+        for (int i=0; i<numAmigos; i++) {
+            String categoria = scan.next();
+            if (recuento.containsKey(categoria)) {
+                //Incrementar el recuento de la categoria
+                recuento.put(categoria, recuento.get(categoria) + 1);
+            } else {
+                //Añadir la categoria al recuento con 1
+                recuento.put(categoria, 1);
+            }
+        }
+
+        //Recorrer el HashMap para buscar las categorias con el minimo número de ocurrencias
+        // que será la que tenga más probabilidades de ganar
+        int minOcurrencias = Integer.MAX_VALUE;
+        List<String> categoriasGanadoras = new ArrayList<>();
+        for (String categoria : recuento.keySet()) {
+            if (recuento.get(categoria).intValue() < minOcurrencias) {
+                minOcurrencias = recuento.get(categoria);
+                categoriasGanadoras = new ArrayList<>();
+                categoriasGanadoras.add(categoria);
+            }
+            else if (recuento.get(categoria) == minOcurrencias) {
+                categoriasGanadoras.add(categoria);
+            }
+        }
+
+        categoriasGanadoras.sort(null);
+
+        for (String categoria : categoriasGanadoras) {
+            System.out.println(categoria);
+        }
+    }
+
+}
+
---
+++ src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_5_TablaDeHash_Facil/GrandpaBernie.java
@@ -0,0 +1,68 @@
+package Cap2._2_EstructurasDatosNoLinealesConBibliotecas._5_TablaDeHash_Facil;
+
+
+import java.io.*;
+import java.util.*;
+
+// Leer los viajes a cada país y apuntarlos en una lista.
+// Ordenar las listas
+// Responder a las preguntas accediendo a la lista de cada país y a la posición indicada
+
+// Caso #3: TLE  -- uso fast io  -> TLE
+//-- ordeno las listas, sólo si se necesita -> TLE
+// copio de https://github.com/shakeelsamsu/kattis/blob/master/src/grandpabernie.java -> AC
+// Falta analizar el código y compararlo con el de la solución de kattis
+//   --> Usar fast output --> TLE
+//   --> Usar StringTokenizer en lugar de .split(" ") --> TLE
+//  --> Usar un HashSet para los países visitados --> TLE
+//  --> Dejo de usar FastIO y uso BufferedReader y PrintWriter  --> AC
+
+public class GrandpaBernie {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        int numViajes = Integer.parseInt(in.readLine());
+
+        HashMap<String, List<Integer>> viajes = new HashMap<String, List<Integer>>();
+
+        for (int i=0; i<numViajes; i++) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            String pais = st.nextToken();
+            int anyo = Integer.parseInt(st.nextToken());
+
+            if (viajes.get(pais)!=null) {
+                //Añadir a la lista de viajes del país
+                viajes.get(pais).add(anyo);
+            } else {
+                //Añadir el pais a la lista con su primer año
+                List<Integer> lista = new ArrayList<>();
+                lista.add(anyo);
+                viajes.put(pais, lista);
+            }
+        }
+
+        //Responder a las preguntas
+        int numPreguntas = Integer.parseInt(in.readLine());
+        HashSet<String> visitados = new HashSet<>();
+        for (int i=0; i<numPreguntas; i++) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            String pais = st.nextToken();
+            int posicion = Integer.parseInt(st.nextToken());
+
+            if (!visitados.contains(pais)) {
+                visitados.add(pais);
+                //Ordenar la lista de viajes del país
+                Collections.sort(viajes.get(pais));
+            }
+            //System.out.println(viajes.get(pais).get(posicion-1));
+            out.println(viajes.get(pais).get(posicion-1));
+        }
+
+        out.close();
+
+    }
+
+}

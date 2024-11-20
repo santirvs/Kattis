0a87a04b9d6a362e53a3165ddec3beade1b4d72d
srivas-ITB <santiago.rivas@itb.cat>
2024-11-20 21:34:14 +0100

    Empiezo BST

--- src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_7_BST_Equilibrado_Set/BinarySearchTree.java
+++ src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_7_BST_Equilibrado_Set/BinarySearchTree.java
@@ -6,106 +6,47 @@ import java.util.*;
 // Crear una estructura BST que, además de guardar los números, acumule la cantidad de niveles que se han recorrido
 // Caso de prueba #5: TLE - Cambio a fast input/output. Sigue el TLE
 
-// Cambio de estrategia:
-// 1.- Crear un Set que me permita saber si un número ya ha sido insertado
-// 2.- Crear un array de int que me permita saber cuántos niveles se han recorrido para insertar un número
-// 3.- Añadir el número al Set y calcular el coste de insertarlo en el BST: coste del anterior o posterior +1
-//     Hay que recordar que un Set siempre está ordenado
-// 4.- Imprimir el coste de insertar el número
+//          Cambio y uso un TreeMap para poder obtener el floor y el ceil de un número (un TreeMap es un BST)
 
 
 public class BinarySearchTree {
 
-     public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         //Scanner scan = new Scanner(System.in);
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
-        int numValores = Integer.parseInt(in.readLine())-1;
 
-        HashSet<Integer> seen = new HashSet<>();
-        int[] dist = new int[numValores+2];
+        TreeMap<Integer, Integer> bst = new TreeMap<>();
+        Integer floor, ceil, profundidad;
 
-        long total = 0;
+        long totalNiveles = 0; //running total
 
-        for (int i = 0; i < numValores; i++) {
-            int t = Integer.parseInt(in.readLine());
-            seen.add(t);
-            seen.iterator()
-            int cost = 0;
-            if(seen.lower(t) != null) {
-                cost = Math.max(dist[seen.lower(t)], cost);
-            }
-            if(seen.higher(t) != null) {
-                cost = Math.max(dist[seen.higher(t)], cost);
-            }
-            total += cost;
-            dist[t] = cost+1;
-            out.println(total);
-        }
+        int numCasos = Integer.parseInt(in.readLine());
 
-        BST arbol = new BST(Integer.parseInt(in.readLine()));
-        int totalNiveles = 0;
-        out.println(totalNiveles);
+        for (int i = 0; i < numCasos; i++) {
+            Integer newNode = Integer.parseInt(in.readLine());
 
-        while (numValores > 0) {
-            totalNiveles += arbol.insert(Integer.parseInt(in.readLine()));
-            out.println(totalNiveles);
-
-            numValores--;
-        }
-
-        out.flush();
-        out.close();
-
-    }
-
-}
-
-/*
-
-import java.io.*;
-import java.util.*;
-
-public class BST {
-  public static void main(String[] args) throws IOException{
-  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-  TreeMap<Integer, Integer> depths = new TreeMap<>();
-
-
-
-        Integer floor, ceil, depth;
-        long totalDepth = 0; //running total
-
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            Integer newNode = Integer.parseInt(br.readLine());
-
-            floor = depths.floorKey(newNode); //parent node if inserted on the right
-            ceil = depths.ceilingKey(newNode); //parent node if inserted on the left
+            floor = bst.floorKey(newNode); //nodo padre si se inserta a la derecha
+            ceil = bst.ceilingKey(newNode); //nodo padre si se inserta a la izquierda
 
             if (floor == null) {
                 if (ceil == null)
-                    depth = 0; //Empty tree
-
+                    profundidad = 0; //Arbol vacío
                 else
-                    depth = depths.get(ceil) + 1; //only ceil has a value
+                    profundidad = bst.get(ceil) + 1; //solo el techo tiene valor
+            } else {
+                if (ceil == null)
+                    profundidad = bst.get(floor) + 1; //solo el suelo tiene valor
+                else
+                    profundidad = Math.max(bst.get(floor), bst.get(ceil)) + 1; // el mayor de los dos es donde se insertará
             }
+            bst.put(newNode, profundidad); //guarda la nueva profundidad en el mapa
 
-            else if (ceil == null)
-                depth = depths.get(floor) + 1; //only floor has a value
+            totalNiveles += profundidad;
+            System.out.println(totalNiveles);
 
-            else
-                depth = Math.max(depths.get(floor), depths.get(ceil)) + 1; //the one with the larger depth is where it will be inserted
-
-            depths.put(newNode, depth); //store new nodes depth in the map
-            totalDepth += depth;
-            System.out.println(totalDepth);
         }
-  }
+    }
 }
-
-*/

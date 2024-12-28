9abcfa405979963f3a6c169dc9f70a5b91946bcf
srivas-ITB <santiago.rivas@itb.cat>
2024-12-28 09:37:51 +0100

    Hasta el FantasyDraft

---
+++ src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_8_BST_Equilibrado_Map/CakeyMcCakeFace.java
@@ -0,0 +1,74 @@
+package Cap2._2_EstructurasDatosNoLinealesConBibliotecas._8_BST_Equilibrado_Map;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+// Caso de prueba #2: WA --> Empezar a contar desde 0 y no desde la diferencia entre los primeros elementos de cada lista
+// Caso de prueba #3: WA --> Corregir el < a <= hasta el último elemento de la lista de salidas
+// Caso de prueba #5: TLE --> No se puede hacer un recorrido de 1000 millones de iteraciones!!!
+//   - Si se calculan las diferencias entre las listas de entradas y salidas, se puede hacer un recorrido de 2000 * 2000 iteraciones
+//     que, aunque lento, entra dentro de lo aceptable
+//   - Miramos la distancia entre todos los elementos de la entrada y la salida
+//   - Vamos contando en un mapa las coincidencias para cada distancia (incremento) incremento -> contador de coincidencias
+//   - Finalmente, recorremos las claves del mapa y buscamos la de mayor coincidencia
+//   - La clave con mayor coincidencia es la distancia que buscamos
+//   --> AC!
+
+public class CakeyMcCakeFace {
+
+    public static void main(String[] args) throws IOException {
+        //Scanner scan = new Scanner(System.in);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        //Lee el numero de personas
+        int numEntradas = Integer.parseInt(in.readLine());
+        int numSalidas = Integer.parseInt(in.readLine());
+
+        //Crea una lista con las salidas y otra lista con las entradas
+        ArrayList<Integer> listaEntradas = new ArrayList<>();
+        ArrayList<Integer> listaSalidas = new ArrayList<>();
+
+        //Lee las entradas y las guarda en una lista
+        String[] partes = in.readLine().split(" ");
+        for (int i = 0; i < numEntradas; i++) {
+            listaEntradas.add(Integer.parseInt(partes[i]));
+        }
+
+        //Lee las salidas y las guarda en una lista
+        partes = in.readLine().split(" ");
+        for (int i = 0; i < numSalidas; i++) {
+            listaSalidas.add(Integer.parseInt(partes[i]));
+        }
+
+        //Registra en un mapa la distancia entre los elementos de la lista de salidas y la lista de entradas
+        TreeMap<Integer, Integer> contadorDistancias = new TreeMap<>();
+        for (int salida : listaSalidas) {
+            for (int entrada : listaEntradas) {
+                int distancia = salida - entrada;
+                if (distancia >= 0)
+                    contadorDistancias.put(distancia, contadorDistancias.getOrDefault(distancia, 0) + 1);
+            }
+        }
+
+        //Busca la distancia con mayor coincidencia
+        int maximo = 0;
+        int distancia = 0;
+        for (int key : contadorDistancias.keySet()) {
+            if (contadorDistancias.get(key) > maximo) {
+                maximo = contadorDistancias.get(key);
+                distancia = key;
+            }
+        }
+
+        System.out.println(distancia);
+
+        out.flush();
+        out.close();
+        in.close();
+    }
+
+}
+
---
+++ src/Cap2/_2_EstructurasDatosNoLinealesConBibliotecas/_8_BST_Equilibrado_Map/FantasyDraft.java
@@ -0,0 +1,117 @@
+package Cap2._2_EstructurasDatosNoLinealesConBibliotecas._8_BST_Equilibrado_Map;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+//  Caso de prueba #48: TLE
+//  No es necesario actualizar la lista de preferencias de los equipos, basta con comprobar
+//  si el jugador sigue disponible en la lista de jugadores pendientes  -> AC, pero Caso #49: TLE
+//  Caso de prueba #49: TLE
+//  Se cambia la lista de jugadores pendientes de ArrayList a TreeMap para acelerar la eliminación de jugadores -> AC
+//
+
+public class FantasyDraft {
+
+    public static void main(String[] args) throws IOException {
+        //Scanner scan = new Scanner(System.in);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        //Lee el numero de personas
+        String[] partes = in.readLine().split(" ");
+        int numTeams = Integer.parseInt(partes[0]);
+        int tamanyoEquipo = Integer.parseInt(partes[1]);
+
+        //Array de mapas para cada equipo
+        Map<Integer, ArrayList<String>> equipos = new TreeMap<>();
+        for (int i = 0; i < numTeams; i++) {
+            equipos.put(i, new ArrayList<>());
+        }
+
+        //Lee los jugadores y los añade a los equipos
+        for (int i = 0; i < numTeams; i++) {
+            partes = in.readLine().split(" ");
+            int numJugadores = Integer.parseInt(partes[0]);
+
+            for (int j = 1; j <= numJugadores; j++) {
+                String nomJugador = partes[j];
+                equipos.get(i).add(partes[j]);
+            }
+        }
+
+        //Lee los jugadores
+        int numJugadores = Integer.parseInt(in.readLine());
+        //Lista de jugadores pendientes en orden de elección general
+        TreeMap<Integer, String> ordenJugadoresPendientes = new TreeMap<>();
+        //TreeMap para guardar los jugadores pendientes de ser elegidos y su posición en la lista general
+        TreeMap<String, Integer> jugadores = new TreeMap<>();
+
+        for (int i = 0; i < numJugadores; i++) {
+            String jugador = in.readLine();
+            jugadores.put(jugador, i);
+            ordenJugadoresPendientes.put(i, jugador);
+        }
+
+        //Empieza el draft
+        //Inicializar la lista de jugadoresElegidos
+        Map<Integer, ArrayList<String>> jugadoresElegidos = new TreeMap<>();
+        for (int i = 0; i < numTeams; i++) {
+            jugadoresElegidos.put(i, new ArrayList<>());
+        }
+
+        //Para cada ronda (tamanyo del equipo)
+        for (int i = 0; i < tamanyoEquipo; i++) {
+            //Para cada equipo
+            for (int j = 0; j < numTeams; j++) {
+                boolean escogido = false;
+                // Si aún quedan jugadores en la lista de preferencias
+                while  (!escogido && equipos.get(j).size() != 0) {
+                    //Saca el primer jugador de la lista de preferencias
+                    String jugador = equipos.get(j).remove(0);
+
+                    //Comprueba si el jugador sigue disponible
+                    if (jugadores.containsKey(jugador)) {
+                        //Lo añade a la lista de jugadores elegidos
+                        jugadoresElegidos.get(j).add(jugador);
+
+                        //Lo elimina de la lista de jugadores pendientes
+                        ordenJugadoresPendientes.remove(jugadores.get(jugador));
+                        jugadores.remove(jugador);
+
+                        escogido = true;
+                    }
+                }
+
+                // Si no ha podido escoger a un jugador de la lista de preferencias de su equipo, se tira de la lista de pendientes
+                if (!escogido) {
+                    String jugador = ordenJugadoresPendientes.firstEntry().getValue();
+                    jugadoresElegidos.get(j).add(jugador);
+
+                    ordenJugadoresPendientes.remove(jugadores.get(jugador));
+                    jugadores.remove(jugador);
+                }
+
+            }
+        }
+
+        //Imprime los jugadores elegidos
+        for (int i = 0; i < numTeams; i++) {
+            boolean primero = true;
+            for (String jugador : jugadoresElegidos.get(i)) {
+                if (!primero) out.print(" ");
+                primero = false;
+                out.print(jugador);
+            }
+            out.println();
+        }
+
+
+
+        out.flush();
+        out.close();
+        in.close();
+    }
+
+}

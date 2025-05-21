c234be2f2e5f35075f79665c8439d82682449af7
srivas-ITB <santiago.rivas@itb.cat>
2025-05-21 15:50:27 +0200

    Inicio JustForSideKicks

--- src/Cap2/_4_EstructurasDatosNoLinealesConBibiliotecasPropias/_3_EstructurasDatosRelativasArboles/JustForSideKicks.java
+++ src/Cap2/_4_EstructurasDatosNoLinealesConBibiliotecasPropias/_3_EstructurasDatosRelativasArboles/JustForSideKicks.java
@@ -15,6 +15,24 @@ public class JustForSideKicks {
     X  X  X  X  X
     Suma = 39+39+10+25+69 = 182
 
+1 1 6  -> cambiar tipo de gema de la P1 al tipo 6
 
+69 39 39 10 25 69 420 30
+
+2 6 3286  --> cambio de valor de la gema de tipo 6 al nuevo valor de 3286
+
+3286 39 39 10 25 3286 420 30
+
+3 2 6
+
+3286 39 39 10 25 3286 420 30
+     X  X  X  X  X
+     Suma = 39+39+10+25+3286 = 3399
+
+3 1 8
+
+3286 39 39 10 25 3286 420 30
+X    X  X  X  X  X    X   X
+     Suma = 3286+39+39+10+25+3286+420+30 = 7135
 
  */
